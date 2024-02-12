517445e1edb5ad188db3ca930a9cf900613aa719
Nima Balazadeh <nimabalazadeh83@gmail.com>
2024-02-12 17:30:52 +0330

    finishing FoodDatabase class everything that we write in companion object{} is available without creating an object of the class in this part we used singleton to create just and only one instance of the Database and having access to it by getDatabase function

--- app/src/main/java/balazadeh/nima/dunifood/Room/FoodDatabase.kt
+++ app/src/main/java/balazadeh/nima/dunifood/Room/FoodDatabase.kt
@@ -1,9 +1,28 @@
 package balazadeh.nima.dunifood.Room
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import balazadeh.nima.dunifood.Food
 
-@Database(version = 1 , exportSchema = false, entities = [Food::class] )
-abstract class FoodDatabase:RoomDatabase() {
+@Database(version = 1, exportSchema = false, entities = [Food::class])
+abstract class FoodDatabase : RoomDatabase() {
+    abstract val foodDao: FoodDao
+
+
+    companion object {
+        private val database: FoodDatabase? = null
+        fun getDatabase(context: Context): FoodDatabase {
+            var instance = database
+            if (instance == null) {
+                instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    FoodDatabase::class.java,
+                    "myDatabase.db"
+                ).build()
+            }
+            return instance
+        }
+    }
 }
